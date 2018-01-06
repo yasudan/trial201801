9da0c536a8101caa11a44bbc6f5a0f345be9a1bc
yasudan <yasuto.fujita@gmail.com>
2018-01-06 16:33:52 +0000

    アニメーション追加

--- trial.js
+++ trial.js
@@ -1,2 +1,10 @@
 'use strict';
+const anime = require('animejs');
+const ball = document.getElementById('ball-area');
+ball.addEventListener('click', function() {
+    anime({
+        targets: ball,
+        translateX: 250
+    })
+});
 console.log("テストだよ");
